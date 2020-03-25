c6ada928919cebe9b71557c235a2e17856826988
Oleksandr Romaniuk <47930925+olksndrdevhub@users.noreply.github.com>
2020-03-25 17:15:43 +0200

    Update wsgi.py to deploy at AWS

--- kinoCMS/wsgi.py
+++ kinoCMS/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+
 import os
-
+import sys
+sys.path.append('/home/ubuntu/django/kinoCMS/kinoCMS')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/home/ubuntu/django/kinoCMS/kinoCMS/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kinoCMS.settings")
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kinoCMS.settings')
-
 application = get_wsgi_application()
